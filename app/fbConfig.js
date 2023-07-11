b3dcc83df76905c583b39266078bd3198a240b55
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-07-11 10:14:22 -0400

    connected to firebase auth

---
+++ app/fbConfig.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBaIlwOTwZM1dTXdyWiRSBFkk_tUwFuEJA",
+    authDomain: "chekov-dl.firebaseapp.com",
+    projectId: "chekov-dl",
+    storageBucket: "chekov-dl.appspot.com",
+    messagingSenderId: "148888952125",
+    appId: "1:148888952125:web:ee12096ff5df55a6683954"
+  };
