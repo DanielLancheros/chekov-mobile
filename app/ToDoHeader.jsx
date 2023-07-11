905a79c52250a7d9dc9f083ffdebc503ad265c27
Daniel Lancheros <daniellancheros@Daniels-MacBook-Air.local>
2023-07-11 11:50:06 -0400

    Added input for adding new item

---
+++ app/ToDoHeader.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import { Input, Button, HStack } from 'native-base'
+
+
+export default function ToDoHeader ({ setToDoItems, user }) {
+    
+    const [newItem, setNewItem] = useState('')
+
+    const addNewItem = () => {
+        if(!newItem.length < 3) return // checks for valid entry
+        const newToDoItem = {
+            uid: user.uid,
+            title: newItem,
+        }
+        fetch(`https://chekov-api-dl.web.app/tasks/${user.uid}`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(newToDoItem),
+    })
+        .then(res => res.json())
+        .then(setToDoItems)
+        .catch(alert)
+    }
+    
+    return (
+        <HStack space={2}>
+            <Input onChangeText={setNewItem} size="lg" color="coolGray.200" flex={1} placeholder='Add Item'/>
+            <Button onPress = {addNewItem}>Add</Button>
+        </HStack>
+    )
+}
--- app/ToDoList.jsx
+++ app/ToDoList.jsx
@@ -1,8 +1,47 @@
-import { Text } from "native-base";
+import { useState, useEffect } from "react";
+import { Center, Box, Heading, VStack, Checkbox, HStack, Text } from "native-base";
+import ToDoHeader from "./ToDoHeader";
 
-export default function ToDoList() {
+export default function ToDoList({ user }) {
+
+    const [toDoItems, setToDoItems] = useState() 
+
+    useEffect(() => {
+        if(user) {
+            fetch(`https://chekov-api-dl.web.app/tasks/${user.uid}`)
+            .then(res => res.json())
+            .then(setToDoItems)
+            .catch(alert)
+        }
+    }, [user])
+
   return (
-     <Text color="coolGray.100" fontSize="3xl" >ToDo List</Text>    
+    <Center w="100%">
+        <Box maxW={300} w='100%'>
+            <VStack space={4} mt={4}>
+                <ToDoHeader user={ user } setToDoItems={ setToDoItems } />
+                {!toDoItems
+                ? <Text fontSize="xl" w="100%" color="coolGrey.300" textAlign="center">Loading...</Text>
+                : toDoItems.map(item => (
+                    <HStack key={item.id} w="100%" justifyContent="space-between" alignItems="center">
+                        <Checkbox 
+                        aria-label={item.title}
+                        isChecked={item.done} />
+                        <Text 
+                            fontSize={18} 
+                            mx={2}
+                            strikeThrough={item.done}
+                            color={item.done ? 'coolGray.500' : "coolGray.100"}
+                            textAlign="left"
+                            width="100%"
+                        
+                        >{item.title}</Text>
+                    </HStack>
+                ))
+                }
+            </VStack>
+        </Box>
+    </Center>    
   );
 }
 
